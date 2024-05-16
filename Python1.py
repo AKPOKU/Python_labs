cbcdfd1387dc46bf14e6344fd2e22473e6d7f02a
AK1993 <akissi.akan@gmail.com>
2024-05-16 18:28:42 -0500

    Feature/Odd/Eeven

--- Python1.py
+++ Python1.py
@@ -160,3 +160,20 @@ else:
     print("Your Number is to low")
 pass
 print("You win")
+
+
+##NOTE
+
+number = 0
+
+if number > 0:
+    print('Positive number')
+
+elif number <0:
+    print('Negative number')
+
+else:
+    print('Zero')
+
+print('This statement is always executed')
+
